f8986041cf73b237ac8243c0cf8ed0db82f4c378
katayama <tattu.0310@gmail.com>
2022-11-06 13:01:50 +0900

    update

--- src/components/sample.tsx
+++ src/components/sample.tsx
@@ -2,7 +2,7 @@ export const Sample = () => {
   const fruits = ["apple", "banana", "orange"];
   return (
     <div>
-      <h1>Sample</h1>
+      <h1 className="text-red-200">Sample</h1>
       <ul>
         {fruits.map((fruit) => (
           <li>{fruit}</li>
